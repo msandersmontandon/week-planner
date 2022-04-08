d509d60454b3607a225516c70837c8389f9daf36
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-08 22:16:38 +0000

    jon commit

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ var data = {
 var modal = document.querySelector('[data-view="modal"]');
 var entryButtonEl = document.querySelector('#entry');
 var submitButtonEl = document.querySelector('#submit');
-
+var days = document.querySelector('#days');
+var time = document.querySelector('#time');
+var description = document.querySelector('#description');
 // function show(event, )
 
 entryButtonEl.addEventListener('click', function (event) {
@@ -16,5 +18,13 @@ entryButtonEl.addEventListener('click', function (event) {
 
 submitButtonEl.addEventListener('submit', function (event) {
   event.preventDefault();
+  var newEntry = {
+    day: days.value,
+    time: time.value,
+    description: description.value,
+    id: data.nextEntryId++
+  };
+  data.entry.unshift(newEntry);
   modal.className = 'hidden';
+  event.target.reset();
 });
